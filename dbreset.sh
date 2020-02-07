86ed0add8eec0fab1ef27dce18f2266f9381a4d1
amakusa0 <usk5571@gmail.com>
2020-02-07 16:15:46 +0900

    add file to .gitignore

--- dbreset.sh
+++
@@ -1,6 +0,0 @@
-rm -r ./questions/migrations
-rm -r ./accounts/migrations
-rm db.sqlite3
-python manage.py makemigrations questions
-python manage.py makemigrations accounts
-python manage.py migrate
